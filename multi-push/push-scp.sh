abc3b5566000d707c5cc2e1f964fd96f1d6ac389
Alen Turkovic <alturkovic@gmail.com>
2018-12-20 15:13:41 +0100

    Update push-scp.sh

--- multi-push/push-scp.sh
+++ multi-push/push-scp.sh
@@ -12,18 +12,18 @@ push() {
 
   # copy as temporary, to prevent the other side from processing unfinished files
   if scp -q -P $_port $_source_directory${_file} ${_user}@${_host}:${_target_absolute_dir}${_file}.tmp; then
-    echo -e "\tINFO - Copy successful for ${_file} @ ${_host}, removing .tmp extension"
+    echo -e "INFO - Copy successful for ${_file} @ ${_host}, removing .tmp extension"
 
     # remove temporary extension to mark copy as complete
     if ssh -l ${_user} ${_host} "mv ${_target_absolute_dir}${_file}.tmp ${_target_absolute_dir}${_file}"; then
       # mark as success, next execution will skip this file for this host
-      echo -e "\tINFO - Push completed for ${_file} @ ${_host}"
+      echo -e "INFO - Push completed for ${_file} @ ${_host}"
       touch $_source_directory${_file}.${_host}.success
     else
-      echo -e "\tERROR - Rename failed for ${_file} @ ${_host}"
+      echo -e "ERROR - Rename failed for ${_file} @ ${_host}"
     fi
 
   else
-    echo -e "\tERROR - Copy failed for ${_file} @ ${_host}"
+    echo -e "ERROR - Copy failed for ${_file} @ ${_host}"
   fi
 }
