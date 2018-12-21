6becec217f8f26dbeaab3d80f2edfe5ef0a017ed
Alen Turkovic <alturkovic@gmail.com>
2018-12-21 09:36:03 +0100

    Update push-sftp.sh

--- multi-push/push-sftp.sh
+++ multi-push/push-sftp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 put_file() {
-  echo "cd ${_target_absolute_dir}"
+  echo "cd ${_target_directory}"
   echo "lcd ${_source_directory}"
   echo "put ${_file}"
 }
@@ -11,12 +11,11 @@ push() {
   if [ -z $2 ]; then echo "Missing file"; return 1; else _file="$2"; fi
   if [ -z $3 ]; then echo "Missing user"; return 1; else _user="$3"; fi
   if [ -z $4 ]; then echo "Missing host"; return 1; else _host="$4"; fi
-  if [ -z $5 ]; then echo "Missing target directory"; return 1; else _target_absolute_dir="$5"; fi
+  if [ -z $5 ]; then echo "Missing target directory"; return 1; else _target_directory="$5"; fi
   if [ -z $6 ]; then _port=22; else _port="$6"; fi
-  if [ -z $7 ]; then _push_batch_script=put_file ; else _push_batch_script=$(read "$7"); fi
 
-  if sftp -oPort=$_port -b <(put_file) ${_user}@${_host}; then
-    echo -e "INFO - Push completed for ${_file} @ ${_host}"
-    touch $_source_directory${_file}.${_host}.success
+  if sftp -o Port=${_port} -b <(put_file) ${_user}@${_host}; then
+    echo "INFO - Push completed for ${_file} @ ${_host}"
+    touch ${_source_directory}${_file}.${_host}.success
   fi
 }
